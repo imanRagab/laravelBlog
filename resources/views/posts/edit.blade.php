7428a4c53db1173adc52e4832ac72d92c3564942
Iman <iman.mansour94@gmail.com>
2018-04-14 00:34:44 +0200

    fix user select list -- modify edit view

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -20,12 +20,16 @@
             <label for="creator">Post Creator</label>
             <select id="creator" name="user_id">
                 @foreach ($users as $user)
-                    <option value="{{ $user->id }}" selected="{{ $post->user->name }}">{{ $user->name }}</option>
+                    @if ($user->id === $post->user->id)
+                        <option value="{{ $user->id }}" selected="selected">{{ $user->name }}</option>
+                    @else
+                        <option value="{{ $user->id }}">{{ $user->name }}</option>
+                    @endif
                 @endforeach
             </select>
             
         </div>
-        <button type="submit" class="btn btn-success btn-lg">Submit</button>
+        <button type="submit" class="btn btn-info btn-lg">Update</button>
 </form>
 
 

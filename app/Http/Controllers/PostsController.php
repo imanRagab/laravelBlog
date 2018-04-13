097986654b600fcb771d4357002de10472b414e6
Iman <iman.mansour94@gmail.com>
2018-04-13 20:46:48 +0200

    add posts controller

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use App\User;
+use Illuminate\Http\Request;
+
+
+class UserController extends Controller
+{
+    /**
+     * Show the profile for the given user.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function index(){
+
+        $posts = Post::all();
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
+
+    }
+}

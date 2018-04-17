179c028d97b050c7da5fae42cbac25f8660a4acc
Iman <iman.mansour94@gmail.com>
2018-04-17 20:21:18 +0200

    add post/user resources

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -8,18 +8,20 @@ use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\PostsStoreRequest;
+use App\Http\Resources\PostResource;
+
 
 class PostsController extends Controller
 {
     public function index(){
 
-        return Post::with('user')->paginate(1);
+        return PostResource::collection(Post::with('user')->paginate(1));
 
     }
 
     public function store(PostsStoreRequest $request){
 
-        return Post::create($request->except(['slug']));
+        return new PostResource(Post::create($request->except(['slug'])));
         
     }
 }
